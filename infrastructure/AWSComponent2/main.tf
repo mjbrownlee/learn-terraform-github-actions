064902c31cf5383e27378164ee0cfccd411d6be2
Mark Brownlee <mark_j_brownlee@hotmail.com>
2024-04-14 16:15:47 -0700

    more changes

---
+++ infrastructure/AWSComponent2/main.tf
@@ -0,0 +1,27 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_ssm_parameter" "foo" {
+    name = "AWSComponent2"
+    type = "String"
+    value = "AWSComponent2 Value"
+}
+
---
+++ infrastructure/AWSComponent3/main.tf
@@ -0,0 +1,27 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_ssm_parameter" "foo" {
+    name = "AWSComponent3"
+    type = "String"
+    value = "AWSComponent3 Value"
+}
+
